b5529b1000d330d0cd0ddcaf4d17033b0fd48faa
bless4 <36552429+bless4@users.noreply.github.com>
2018-10-16 13:56:36 +0200

    first commit

---
+++ assests/js/todo.js
@@ -0,0 +1,27 @@
+//Check off Specific TODO by Clicking
+$( "ul" ).on("click", "li",  function() {
+   $(this).toggleClass("completed");
+});
+
+//click on X to delete TODO
+$("ul").on("click", "span", function(event){
+$(this).parent().fadeOut(500, function(){
+$(this).remove();
+});
+
+ event.stopPropagation();
+});
+ //grabbing text, new todo input
+ $("input[type='text']").keypress (function(event){
+      if (event.which===13){
+          var todoText= $(this).val();
+        $(this).val("");
+          //create a new li and add to ul
+ $("ul").append("<li><span><i class='fa fa-trash'></i></span> " +  todoText + "</li>")
+      }
+ });
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+
+})
